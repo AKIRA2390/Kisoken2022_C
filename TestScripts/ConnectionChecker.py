8df98cef7be1e054a3ef19cfb952b8ea07dd40c3
akira2390 <ando--akira@outlook.com>
2022-07-07 00:35:10 +0900

    自宅でのテスト用に簡易版serverを作った　動くのかは知らん

---
+++ TestScripts/ConnectionChecker.py
@@ -0,0 +1,49 @@
+import socket
+import json
+import pprint
+
+HOST = '127.0.0.1'
+PORT = 8893
+BUFFER_SIZE = 1024
+
+
+def exec_cmd(c):
+    if 'command' not in c:
+        msg = 'KeyError : Not found "command" key.'
+        print(msg)
+        return dict(status='Error', msg=msg)
+    try:
+        pprint.pprint(c)
+    except KeyError:
+        cmd_arg = [s for s in c.keys() if s != 'command']
+        msg = 'Invalid argument : "{0}" -> {{{1}}}'.format(
+            c['command'], ", ".join(cmd_arg))
+        print(msg)
+        res = dict(is_sccess=False, msg=msg)
+
+    return res
+
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(3)
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            print("running...")
+            c, client = s.accept()
+            print('Client connected {0}'.format(client))
+            try:
+                cmd_dict = json.loads(c.recv(BUFFER_SIZE).decode('UTF-8'))
+                res = exec_cmd(cmd_dict)
+                print(res)
+                c.send(json.dumps(res, ensure_ascii=False).encode())
+            finally:
+                c.close()
+                if 'command' in cmd_dict:
+                    if cmd_dict['command'] == 'Quit':
+                        break
+except    TimeoutError:
+    print("timeout")
+finally:
+    s.close()
