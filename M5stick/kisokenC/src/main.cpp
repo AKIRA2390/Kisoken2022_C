95576cb83f5e80b21a5d35494f0b67eae281167d
akira2390 <ando--akira@outlook.com>
2022-10-24 23:35:29 +0900

    yada yada yada

--- M5stick/kisokenC/src/main.cpp
+++ M5stick/kisokenC/src/main.cpp
@@ -1,10 +1,20 @@
-#include <M5StickCPlus.h>
+#include <M5Unified.h>
 // #include <Arduino.h>
+#include <ArduinoJson.h>>
+#include <ESP32Servo.h>
 #include "WiFi.h"
 
 const char* WIFI_SSID = "ipad_akira";
 const char* WIFI_PASS = "kisokenCWiFi";
+const uint16_t port = 8893;
+const char* host = "192.168.1.100";
 WiFiClient client;
+StaticJsonDocument<1024> json;
+
+Servo GripServo;
+Servo CounterServo;
+// void MOVJ(StaticJsonDocument<1024>* json, int x, int y, int z,
+//           bool isrightOrientation);
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,7 +23,8 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  WiFi.config(IPAddress(192,168,1,101),IPAddress(192, 168, 1, 1),IPAddress(255, 255, 0, 0));
+  WiFi.config(IPAddress(192, 168, 1, 101), IPAddress(192, 168, 1, 1),
+              IPAddress(255, 255, 0, 0));
 
   while (WiFi.status() != WL_CONNECTED) {
     if (WiFi.status() == WL_CONNECT_FAILED) {
@@ -27,18 +38,32 @@ void setup() {
     }
     delay(5000);
   }
-
-  M5.Lcd.setCursor(7, 20, 2);
-  m5.Lcd.fillScreen(BLUE);
-  M5.Lcd.print(WiFi.localIP());
   Serial.begin(115200);
+  pinMode(26,OUTPUT);
+  GripServo.attach(26);
+  CounterServo.attach(36);
+  GripServo.write(180);
 }
 
 void loop() {
   // put your main
   M5.update();
-  if(M5.BtnA.wasPressed()){
 
-    client.write()
+  if (!client.connect(host, port)) {
+    Serial.println("Connection failed.");
+    Serial.println("Waiting 5 seconds before retrying...");
+    delay(5000);
+    return;
+  }
+
+  json["command"] = "JumpTo";
+  json["x"] = 0;
+  json["y"] = 0;
+  json["z"] = 70;
+  json["r"] = 0;
+  char data[500];
+  serializeJson(json, data);
+  if (M5.BtnA.wasPressed()) {
+    client.write(data);
   }
 }
