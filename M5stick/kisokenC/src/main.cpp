d6a50937d372456591e1c4224c79c5623bced09a
akira2390 <ando--akira@outlook.com>
2022-10-29 13:00:56 +0900

    20221028

--- M5stick/kisokenC/src/main.cpp
+++ M5stick/kisokenC/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
+#include <FastLed.h>
 #include <M5Unified.h>
 #include <Servo.h>
 
@@ -10,16 +11,35 @@ const char* WIFI_PASS = "kisokenCWiFi";
 const uint16_t port = 8893;
 const char* host = "192.168.1.100";
 WiFiClient client;
+CRGB led[1];
+
+typedef struct coordinate_t {
+  int x, y, r;
+} coordinates;
+
+const enum coordinates {
+  z_Catch = 60,
+  z_Move = 70,
+  z_ServerInit = 100,
+  z_ServerExec = 90,
+};
 
 Servo GripServo;
 Servo CounterServo;
+coordinate_t A1 = {10, 10, 10};
+void sendJson(StaticJsonDocument<1024>);
 void MOVJ(int x, int y, int z, int r);
+void MOVJ(coordinate_t xyr, int z);
 void MainMovement();
 void TestMovement();
 
 void setup() {
   // put your setup code here, to run once:
   M5.begin();
+  FastLED.addLeds<SK6812, 27, RGB>(led, 1);
+
+  led[0] = HUE_ORANGE;
+  FastLED.show();
 
   // WiFi.mode(WIFI_STA);
   // WiFi.disconnect();
@@ -39,6 +59,8 @@ void setup() {
   //   }
   //   delay(5000);
   // }
+  led[0] = HUE_GREEN;
+  FastLED.show();
   Serial.begin(115200);
   pinMode(26, OUTPUT);
   pinMode(18, OUTPUT);
@@ -55,30 +77,44 @@ void loop() {
   }
 }
 
-void MOVJ(int x, int y, int z, int r) {
+void sendJson(StaticJsonDocument<1024> json) {
   if (!client.connect(host, port)) {
     Serial.println("Connection failed.");
     Serial.println(host);
     Serial.println(port);
-    // Serial.println("Waiting 5 seconds before retrying...");
-    // delay(5000);
     return;
   }
-  StaticJsonDocument<1024> json;
-  json["command"] = "JumpTo";
-  json["x"] = x;
-  json["y"] = y;
-  json["z"] = z;
-  json["r"] = r;
   char data[500];
   serializeJson(json, data);
   client.write(data);
   json.clear();
 }
 
-void MainMovement() {
-
+void MOVJ(int x, int y, int z, int r) {
+  StaticJsonDocument<1024> json;
+  json["command"] = "JumpTo";
+  json["x"] = x;
+  json["y"] = y;
+  json["z"] = z;
+  json["r"] = r;
+  sendJson(json);
+  json.clear();
 }
-void TestMovement() {
-
+void MOVJ(coordinate_t xyr, int z){
+  StaticJsonDocument<1024> json;
+  json["command"] = "JumpTo";
+  json["x"] = xyr.x;
+  json["y"] = xyr.y;
+  json["z"] = z;
+  json["r"] = xyr.r;
+  sendJson(json);
+  json.clear();
 }
+
+void MainMovement() {}
+void TestMovement() {}
+
+/*
+座標系のめっも
+
+*/
