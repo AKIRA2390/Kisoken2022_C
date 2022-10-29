262797fbf15c043b814ae640f60ea806b2d31f60
akira2390 <ando--akira@outlook.com>
2022-10-29 17:21:56 +0900

    なんか怪しい動きをした

--- M5stick/kisokenC/src/main.cpp
+++ M5stick/kisokenC/src/main.cpp
@@ -12,26 +12,53 @@ const uint16_t port = 8893;
 const char* host = "192.168.1.100";
 WiFiClient client;
 CRGB led[1];
+bool ExecFlagMain = false, ExecFlagTest = false;
 
 typedef struct coordinate_t {
   int x, y, r;
 } coordinates;
 
-const enum coordinates {
-  z_Catch = 60,
-  z_Move = 70,
-  z_ServerInit = 100,
-  z_ServerExec = 90,
+int TrayPos[3][2][3] = {
+    // TrayNum, PrePost, xyr
+    {{158, 317, 135}, {158, -317, -135}},
+    {{264, 145, 135}, {264, -145, -135}},
+    {{399, -24, 135}, {399, 24, -135}},
+};
+
+int ServerPos[3] = {285, 276, -45};
+
+enum class Zs {
+  z_Move = 140,
+  z_Catch = 115,
+  Z_Carry = 160,
+  z_Release = 115,
+  z_ServerInit = 180,
+  z_ServerExec = 155,
 };
 
 Servo GripServo;
 Servo CounterServo;
-coordinate_t A1 = {10, 10, 10};
+const coordinate_t pos_PreA = {158, 317, 135};
+const coordinate_t pos_PreAu = {158, 317, 135};
+const coordinate_t pos_PostA = {158, -317, -135};
+const coordinate_t pos_PostAu = {168, -307, -135};
+const coordinate_t pos_PreB = {264, 145, 135};
+const coordinate_t pos_PreBu = {264, 145, 135};
+const coordinate_t pos_PostB = {264, -145, -135};
+const coordinate_t pos_PostBu = {264, -145, -135};
+const coordinate_t pos_PreC = {399, -24, 135};
+const coordinate_t pos_PreCu = {399, -24, 135};
+const coordinate_t pos_PostC = {399, 24, -135};
+const coordinate_t pos_PostCu = {399, 24, -135};
+
 void sendJson(StaticJsonDocument<1024>);
 void MOVJ(int x, int y, int z, int r);
-void MOVJ(coordinate_t xyr, int z);
+void MOVJ(const coordinate_t xyr, int z);
+void ArmOrientation(int isItRight);
+void WaitDobot(int millisec);
 void MainMovement();
 void TestMovement();
+void delaye(int duration);
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,24 +68,24 @@ void setup() {
   led[0] = HUE_ORANGE;
   FastLED.show();
 
-  // WiFi.mode(WIFI_STA);
-  // WiFi.disconnect();
-  // WiFi.begin(WIFI_SSID, WIFI_PASS);
-  // WiFi.config(IPAddress(192, 168, 1, 101), IPAddress(192, 168, 1, 1),
-  //             IPAddress(255, 255, 0, 0));
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  WiFi.config(IPAddress(192, 168, 1, 101), IPAddress(192, 168, 1, 1),
+              IPAddress(255, 255, 0, 0));
 
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   if (WiFi.status() == WL_CONNECT_FAILED) {
-  //     Serial.println("Wifi connection failed: ");
-  //     Serial.println();
-  //     Serial.print("SSID: ");
-  //     Serial.println(WIFI_SSID);
-  //     Serial.print("Password: ");
-  //     Serial.println(WIFI_PASS);
-  //     Serial.println();
-  //   }
-  //   delay(5000);
-  // }
+  while (WiFi.status() != WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECT_FAILED) {
+      Serial.println("Wifi connection failed: ");
+      Serial.println();
+      Serial.print("SSID: ");
+      Serial.println(WIFI_SSID);
+      Serial.print("Password: ");
+      Serial.println(WIFI_PASS);
+      Serial.println();
+    }
+    delay(5000);
+  }
   led[0] = HUE_GREEN;
   FastLED.show();
   Serial.begin(115200);
@@ -70,11 +97,21 @@ void setup() {
 
 void loop() {
   M5.update();
+  // GripServo.write(70);
+  // GripServo.write(150);
 
-  if (M5.BtnA.wasPressed()) {
-    // MainMovement();
-    TestMovement();
-  }
+  TestMovement();
+
+  // if (M5.BtnA.wasPressed()) {
+  //   ExecFlagMain = true;
+  //   ExecFlagTest = true;
+  // }
+  // if (ExecFlagMain) {
+  //   MainMovement();
+  // }
+  // if (ExecFlagTest) {
+  //   TestMovement();
+  // }
 }
 
 void sendJson(StaticJsonDocument<1024> json) {
@@ -100,7 +137,7 @@ void MOVJ(int x, int y, int z, int r) {
   sendJson(json);
   json.clear();
 }
-void MOVJ(coordinate_t xyr, int z){
+void MOVJ(coordinate_t xyr, int z) {
   StaticJsonDocument<1024> json;
   json["command"] = "JumpTo";
   json["x"] = xyr.x;
@@ -110,11 +147,63 @@ void MOVJ(coordinate_t xyr, int z){
   sendJson(json);
   json.clear();
 }
+void ArmOrientation(int isItRight){
+  StaticJsonDocument<1024> json;
+  json["command"] = "ArmOrientation";
+  json["mode"] = isItRight;
+  sendJson(json);
+  json.clear();}
+void WaitDobot(int millisec){
+  StaticJsonDocument<1024> json;
+  json["command"] = "Wait";
+  json["ms"] = millisec;
+  sendJson(json);
+  json.clear();}
 
-void MainMovement() {}
-void TestMovement() {}
+void MainMovement() {
+  static uint64_t StartTime = millis();
+  uint64_t ExecTime = StartTime - millis();
+}
+void TestMovement() {
+  WaitDobot(1000);
+  GripServo.write(70);
+  ArmOrientation(1);
+  MOVJ(pos_PreA, int(Zs::z_Move));
+  ArmOrientation(1);
+  MOVJ(pos_PreA, int(Zs::z_Catch));
+  WaitDobot(1000);
+  GripServo.write(150);
+  ArmOrientation(1);
+  MOVJ(pos_PreA, int(Zs::Z_Carry));
+
+  ArmOrientation(0);
+  MOVJ(pos_PostA, int(Zs::Z_Carry));
+  ArmOrientation(0);
+  MOVJ(pos_PostA, int(Zs::z_Catch));
+  WaitDobot(1000);
+  GripServo.write(70);
+  ArmOrientation(0);
+  MOVJ(pos_PostAu, int(Zs::Z_Carry));
+}
 
 /*
-座標系のめっも
+座�
 
+// void delaye(int duration){
+//   static uint64_t _startTime = millis();
+//   uint64_t _execTime = _startTime-millis();
+
+// }
 */
+
+
+
+
+
+
+
+
+
+
+
+
