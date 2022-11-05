b0708da937541fba9fadf5e6c8fa48b5fec255e5
akira2390 <ando--akira@outlook.com>
2022-11-05 14:42:33 +0900

    waaaaa

--- M5stick/kisokenC/src/main.cpp
+++ M5stick/kisokenC/src/main.cpp
@@ -28,30 +28,35 @@ int TrayPos[3][2][3] = {
 int ServerPos[3] = {285, 275, -45};
 
 enum class Zs {
-  Z_Move = 140,
-  Z_Catch = 115,
-  Z_Carry = 160,
-  Z_Release = 115,
-  Z_ServerInit = 180,
-  Z_ServerExec = 155,
+  Move = 140,
+  Pour = 105,
+  Catch = 110,
+  Carry = 160,
+  Release = 115,
+  ServerInit = 185,
+  ServerExec = 155,
+};
+enum class GripAngle {
+  Release = 70,
+  Catch = 170,
 };
 
 Servo GripServo;
 Servo CounterServo;
 const coordinate_t pos_Server = {285, 275, -45};
-const coordinate_t pos_Serveru = {285, 275, -45};
-const coordinate_t pos_PreA = {158, 317, 135};
-const coordinate_t pos_PreAu = {148, 307, 135};
-const coordinate_t pos_PostA = {158, -317, -135};
-const coordinate_t pos_PostAu = {168, -307, -135};
-const coordinate_t pos_PreB = {264, 145, 135};
-const coordinate_t pos_PreBu = {254, 135, 135};
-const coordinate_t pos_PostB = {264, -145, -135};
-const coordinate_t pos_PostBu = {274, -135, -135};
-const coordinate_t pos_PreC = {399, -24, 135};
-const coordinate_t pos_PreCu = {389, -34, 135};
-const coordinate_t pos_PostC = {399, 24, -135};
-const coordinate_t pos_PostCu = {409, 34, -135};
+const coordinate_t pos_Serveru = {235, 225, -45};
+const coordinate_t pos_PreA = {155, 315, 135};
+const coordinate_t pos_PourA = {230, 285, 45};  //+75,-30
+const coordinate_t pos_PostA = {155, -315, -135};
+const coordinate_t pos_PostAu = {165, -305, -135};
+const coordinate_t pos_PreB = {275, 145, 135};
+const coordinate_t pos_PourB = {365, 155, 45};
+const coordinate_t pos_PostB = {275, -145, -135};
+const coordinate_t pos_PostBu = {285, -135, -135};
+const coordinate_t pos_PreC = {395, -25, 135};
+const coordinate_t pos_PourC = {295, 50, 225};
+const coordinate_t pos_PostC = {395, 25, 225};
+const coordinate_t pos_PostCu = {385, 35, 225};
 
 void sendJson(StaticJsonDocument<1024>);
 void MOVJ(int x, int y, int z, int r);
@@ -62,6 +67,11 @@ void WaitDobot(int millisec);
 void MainMovement();
 void TestMovement();
 void delaye(int duration);
+void ServerExec();
+void PourBeads();
+void CarryA();
+void CarryB();
+void CarryC();
 
 void setup() {
   // put your setup code here, to run once:
@@ -89,26 +99,29 @@ void setup() {
     delay(5000);
   }
   Serial.begin(115200);
-  pinMode(36, INPUT_PULLUP);
+  pinMode(19, INPUT_PULLUP);
   pinMode(26, OUTPUT);
   pinMode(18, OUTPUT);
   GripServo.attach(26);
   CounterServo.attach(18);
+  GripServo.write(GripAngle::Catch);
+  CounterServo.write(0);
 }
 
 void loop() {
   // M5.update();
-  // GripServo.write(70);
-  // GripServo.write(150);
-  if (digitalRead(36) == HIGH) {
+  // GripServo.write(70); //release:70
+  // GripServo.write(170);  // catch:150
+  if (digitalRead(19) == LOW) {
     // ExecFlagMain = true;
-    TestMovement();
-    ExecFlagTest = true;
-  // }
-  // if (ExecFlagMain) {
-  //   MainMovement();
-  // } else if (ExecFlagTest) {
-  //   TestMovement();
+    MainMovement();
+    // TestMovement();
+    // ExecFlagTest = true;
+    // }
+    // if (ExecFlagMain) {
+    //   MainMovement();
+    // } else if (ExecFlagTest) {
+    //   TestMovement();
   } else {
     led[0] = CRGB::Orange;
     FastLED.show();
@@ -120,15 +133,19 @@ void sendJson(StaticJsonDocument<1024> json) {
     Serial.println("Connection failed.");
     Serial.println(host);
     Serial.println(port);
+    led[0] = CRGB::Red;
+    FastLED.show();
     return;
   }
   char data[500];
   serializeJson(json, data);
   client.write(data);
   json.clear();
+  while (!client.available());
 }
 
 void MOVJ(int x, int y, int z, int r) {
+  Serial.println("MOVJ");
   StaticJsonDocument<1024> json;
   json["command"] = "JumpTo";
   json["x"] = x;
@@ -139,6 +156,7 @@ void MOVJ(int x, int y, int z, int r) {
   json.clear();
 }
 void MOVJ(coordinate_t xyr, int z) {
+  Serial.println("MOVJ");
   StaticJsonDocument<1024> json;
   json["command"] = "JumpTo";
   json["x"] = xyr.x;
@@ -150,6 +168,7 @@ void MOVJ(coordinate_t xyr, int z) {
 }
 
 void ArmOrientation(int isItRight) {
+  Serial.println("ArmOrientatoin");
   StaticJsonDocument<1024> json;
   json["command"] = "ArmOrientation";
   json["mode"] = int(isItRight);
@@ -157,6 +176,7 @@ void ArmOrientation(int isItRight) {
   json.clear();
 }
 void WaitDobot(int millisec) {
+  Serial.println("WaitDobot");
   StaticJsonDocument<1024> json;
   json["command"] = "Wait";
   json["ms"] = millisec;
@@ -180,36 +200,159 @@ void WaitDobot(int millisec) {
 void MainMovement() {
   static uint64_t StartTime = millis();
   uint64_t ExecTime = StartTime - millis();
+
+  ServerExec();
+  PourBeads();
+  CarryA();
+  CarryC();
+  CarryB();
   ExecFlagMain = 0;
+  ArmOrientation(1);
+  MOVJ(pos_Serveru, int(Zs::ServerInit));
 }
 void TestMovement() {
   led[0] = CRGB::Green;
   FastLED.show();
-  WaitDobot(1000);
+  // ServerExec();
+  // PourBeads();
+  // CarryA();
+  // CarryB();
+  // CarryC();
+
+  ArmOrientation(1);
+  MOVJ(pos_Serveru, int(Zs::ServerInit));
   // delay(2000);
-  GripServo.write(70);
+  // WaitDobot(1000);
+}
+
+void ServerExec() {
   ArmOrientation(1);
-  MOVJ(pos_PreA, int(Zs::Z_Move));
+  MOVJ(pos_Serveru, int(Zs::ServerInit));
   ArmOrientation(1);
-  MOVJ(pos_PreA, int(Zs::Z_Catch));
+  MOVJ(pos_Server, int(Zs::ServerInit));
+  ArmOrientation(1);
+  MOVJ(pos_Server, int(Zs::ServerExec));
+  WaitDobot(2000);
+  ArmOrientation(1);
+  MOVJ(pos_Server, int(Zs::ServerInit));
+  ArmOrientation(1);
+  MOVJ(pos_Serveru, int(Zs::ServerInit));
+}
+void PourBeads() {
+  int delaytime = 750;
+  ArmOrientation(1);
+  MOVJ(pos_PourA, int(Zs::ServerExec));
+  ArmOrientation(1);
+  MOVJ(pos_PourA, int(Zs::Pour));
+  WaitDobot(delaytime * 11);
+  // delay(900);
+  for (int i = 0; i < 10; i++) {
+    CounterServo.write((i % 2) ? 180 : 0);
+    delay(delaytime);
+  }
+  ArmOrientation(1);
+  MOVJ(pos_PourA, int(Zs::ServerExec));
+  ArmOrientation(1);
+  MOVJ(pos_PourB, int(Zs::ServerExec));
+  ArmOrientation(1);
+  MOVJ(pos_PourB, int(Zs::Pour));
+  WaitDobot(delaytime * 11);
+  // delay(900);
+  for (int i = 0; i < 10; i++) {
+    CounterServo.write((i % 2) ? 180 : 0);
+    delay(delaytime);
+  }
+  ArmOrientation(1);
+  MOVJ(pos_PourB, int(Zs::ServerExec));
+  ArmOrientation(1);
+  MOVJ(pos_PourC, int(Zs::ServerExec));
+  ArmOrientation(1);
+  MOVJ(pos_PourC, int(Zs::Pour));
+  WaitDobot(delaytime * 11);
+  // delay(900);
+  for (int i = 0; i < 10; i++) {
+    CounterServo.write((i % 2) ? 180 : 0);
+    delay(delaytime);
+  }
+  ArmOrientation(1);
+  MOVJ(pos_PourC, int(Zs::ServerExec));
+}
+void CarryA() {
+  GripServo.write(int(GripAngle::Release));
+  ArmOrientation(1);
+  MOVJ(pos_PreA, int(Zs::Move));
+  ArmOrientation(1);
+  MOVJ(pos_PreA, int(Zs::Catch));
   WaitDobot(1000);
   delay(2000);
-  GripServo.write(150);
+  GripServo.write(int(GripAngle::Catch));
   ArmOrientation(1);
-  MOVJ(pos_PreA, int(Zs::Z_Carry));
+  MOVJ(pos_PreA, int(Zs::Carry));
 
   ArmOrientation(0);
-  MOVJ(pos_PostA, int(Zs::Z_Carry));
+  MOVJ(pos_PostA, int(Zs::Carry));
   ArmOrientation(0);
-  MOVJ(pos_PostA, int(Zs::Z_Release));
-  WaitDobot(1000);
-  delay(2000);
-  GripServo.write(70);
+  MOVJ(pos_PostA, int(Zs::Release));
+  WaitDobot(300);
+  delay(3500);
+  GripServo.write(int(GripAngle::Release));
   ArmOrientation(0);
-  MOVJ(pos_PostAu, int(Zs::Z_Carry));
+  MOVJ(pos_PostAu, int(Zs::Release));
+  ArmOrientation(0);
+  MOVJ(pos_PostAu, int(Zs::Carry));
   ExecFlagTest = 0;
 }
 
+void CarryB() {
+  GripServo.write(int(GripAngle::Release));
+  ArmOrientation(1);
+  MOVJ(pos_PreB, int(Zs::Move));
+  ArmOrientation(1);
+  MOVJ(pos_PreB, int(Zs::Catch));
+  WaitDobot(1000);
+  delay(2000);
+  GripServo.write(int(GripAngle::Catch));
+  ArmOrientation(1);
+  MOVJ(pos_PreB, int(Zs::Carry));
+
+  ArmOrientation(0);
+  MOVJ(pos_PostB, int(Zs::Carry));
+  ArmOrientation(0);
+  MOVJ(pos_PostB, int(Zs::Release));
+  WaitDobot(300);
+  delay(5000);
+  GripServo.write(int(GripAngle::Release));
+  ArmOrientation(0);
+  MOVJ(pos_PostBu, int(Zs::Release));
+  ArmOrientation(0);
+  MOVJ(pos_PostBu, int(Zs::Carry));
+  ExecFlagTest = 0;
+}
+void CarryC() {
+  GripServo.write(int(GripAngle::Release));
+  ArmOrientation(1);
+  MOVJ(pos_PreC, int(Zs::Move));
+  ArmOrientation(1);
+  MOVJ(pos_PreC, int(Zs::Catch));
+  WaitDobot(1000);
+  delay(2000);
+  GripServo.write(int(GripAngle::Catch));
+  ArmOrientation(1);
+  MOVJ(pos_PreC, int(Zs::Carry));
+
+  ArmOrientation(0);
+  MOVJ(pos_PostC, int(Zs::Carry));
+  ArmOrientation(0);
+  MOVJ(pos_PostC, int(Zs::Release));
+  WaitDobot(300);
+  delay(3500);
+  GripServo.write(int(GripAngle::Release));
+  ArmOrientation(1);
+  MOVJ(pos_PostCu, int(Zs::Release));
+  ArmOrientation(1);
+  MOVJ(pos_PostCu, int(Zs::Carry));
+  ExecFlagTest = 0;
+}
 /*
 座�
 
